0b1b4300c64607dd0d3a36f5d7ac3be22994e1fd
R-ZERO <rodrigo.alan.m.c@live.com>
2019-02-10 05:29:41 -0200

    Add files via upload

---
+++ 01-install-linux-zen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+################################################################################
+# Author	:	Erik Dubois					       #
+# Modicificacão :       Rodrigo Alan M. Carvalho			       #
+################################################################################
+
+echo "Instalando o  Kernel Zen."
+
+sudo pacman -S --noconfirm --needed linux-zen linux-zen-headers
+sudo pacman -S --noconfirm --needed linux-zen-headers
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "###############################################################"
+echo "#### Reiniciar e escolher no grub depois usar o SCRIPT-000 ####"
+echo "###############################################################"
---
+++ 03-install-gpu-and-vulkan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+################################################################################
+# Author	:	Erik Dubois					       #
+# Modicificacão :       Rodrigo Alan M. Carvalho			       #
+################################################################################
+
+
+sudo pacman -S nvidia-dkms --noconfirm --needed
+sudo pacman -S nvidia-utils --noconfirm --needed
+sudo pacman -S lib32-nvidia-utils --noconfirm --needed
+sudo pacman -S opencl-nvidia --noconfirm --needed
+sudo pacman -S lib32-opencl-nvidia --noconfirm --needed
+sudo pacman -S nvidia-settings --noconfirm --needed
+sudo pacman -S vulkan-icd-loader --noconfirm --needed
+sudo pacman -S lib32-vulkan-icd-loader --noconfirm --needed
+
+echo "################################################################"
+echo "#########           You got to reboot.                 #########"
+echo "################################################################"
