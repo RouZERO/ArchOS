12686191a7ca34d72aea445fa0856902585b0836
Rodrigo Carvalho <rodrigo.alan.m.c@gmail.com>
2019-05-31 00:49:51 -0300

    22W2019

---
+++ 03-install-APPARENCE.sh
@@ -0,0 +1,27 @@
+ #!/bin/bash
+set -e
+################################################################################
+# Author	:	Erik Dubois					       #
+# Modicificacão :       Rodrigo Alan M. Carvalho			       #
+################################################################################
+
+
+echo "################################################################"
+echo "####        Intalando Fontes, Icones e Temas                ####"
+echo "################################################################"
+
+[ -d $HOME"/.fonts" ] || mkdir -p $HOME"/.fonts"
+[ -d $HOME"/.icons" ] || mkdir -p $HOME"/.icons"
+[ -d $HOME"/.themes" ] || mkdir -p $HOME"/.themes"
+
+cp -R APPARENCE/fonts/* ~/.fonts/
+cp -R APPARENCE/icons/* ~/.icons/
+cp -R APPARENCE/themes/* ~/.themes/
+
+echo "Building new fonts into the cache files";
+echo "Depending on the number of fonts, this may take a while..."
+fc-cache -fv ~/.fonts
+
+echo "################################################################"
+echo "####     Fonts from Arch Linux repo have been installed     ####"
+echo "################################################################"
