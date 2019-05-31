2c3f2050b71ea09f34a6435f36a4039c21832f30
Rodrigo Carvalho <rodrigo.alan.m.c@gmail.com>
2019-05-31 01:36:30 -0300

    22W2019

--- 01-install-DISPLAY-SERVER.sh
+++ 01-install-DISPLAY-SERVER.sh
@@ -8,22 +8,8 @@ set -e
 sudo pacman -S xorg --noconfirm --needed
 sudo pacman -S xorg-xinit --noconfirm --needed
 sudo pacman -S xorg-server --noconfirm --needed
-
-sudo pacman -S gnome-shell --noconfirm --needed
-sudo pacman -S gnome-tweak-tool --noconfirm --needed
-sudo pacman -S gnome-control-center --noconfirm --needed
-sudo pacman -S gnome-keyring --noconfirm --needed
-sudo pacman -S nautilus --noconfirm --needed
-sudo pacman -S gedit --noconfirm --needed
-sudo pacman -S gdm --noconfirm --needed
-
 sudo pacman -S xdg-user-dirs --noconfirm --needed
-
 sudo pacman -S intel-ucode --noconfirm --needed
-sudo pacman -S tilix --noconfirm --needed
-
-sudo systemctl enable gdm
-
 
 echo "#########################################################################"
 echo "##############           You got to reboot.                 #############"
