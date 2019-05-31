7b0ba7c26235ac7e42796045746d1061466125a2
Rodrigo Carvalho <rodrigo.alan.m.c@gmail.com>
2019-05-31 01:31:52 -0300

    22W2019

---
+++ 02-install-DESKTOP-ENVIRONMENT.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+################################################################################
+# Author	:	Erik Dubois					       #
+# Modicificacão :       Rodrigo Alan M. Carvalho			       #
+################################################################################
+
+sudo pacman -S gnome-shell --noconfirm --needed
+sudo pacman -S gnome-tweak-tool --noconfirm --needed
+sudo pacman -S gnome-control-center --noconfirm --needed
+sudo pacman -S gnome-keyring --noconfirm --needed
+sudo pacman -S nautilus --noconfirm --needed
+sudo pacman -S gedit --noconfirm --needed
+sudo pacman -S gdm --noconfirm --needed
+sudo pacman -S tilix --noconfirm --needed
+sudo systemctl enable gdm
+
+echo "#########################################################################"
+echo "##############           You got to reboot.                 #############"
+echo "#########################################################################"
