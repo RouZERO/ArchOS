defe3d987b1a948ef111f1bc40242adbb4756d84
RouZERO <rodrigo.alan.m.c@live.com>
2021-04-03 10:07:26 -0300

    Update install-uefi.sh

--- install-uefi.sh
+++ install-uefi.sh
@@ -10,7 +10,6 @@ echo "ArchLinux" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 ArchLinux.localdomain ArchLinux" >> /etc/hosts
-echo root:password | chpasswd
 
 pacman -S --noconfirm grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector tlp virt-manager qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables iptables ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g
 
@@ -21,18 +20,14 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
-#systemctl enable cups
-#systemctl enable sshd
 systemctl enable avahi-daemon
 systemctl enable tlp
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable libvirtd
 systemctl enable firewalld
-#systemctl enable acpid
 
 useradd -m rouzero
-echo rouzero:password | chpasswd
 usermod -aG libvirt rouzero
 
 echo "rouzero ALL=(ALL) ALL" >> /etc/sudoers.d/rouzero
