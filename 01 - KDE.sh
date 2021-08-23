77eff11e42394c4ced0768798d8db7a3e415a3c0
RouZERO <rodrigo.alan.m.c@live.com>
2021-08-23 10:17:48 -0300

    Update 01 - KDE.sh

--- 01 - KDE.sh
+++ 01 - KDE.sh
@@ -4,7 +4,7 @@ set -e
 
 
 #sudo pacman -S sddm plasma ark kdeconnect plasma-wayland-session  --noconfirm --needed
-sudo pacman -S plasma konsole dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5 sddm alsa-utils bluez bluez-utils --noconfirm --needed
+sudo pacman -S plasma konsole dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5 sddm alsa-utils --noconfirm --needed
 
 sudo systemctl enable sddm
 sudo systemctl enable bluetooth.service
