f7c76b469704d2df15b5a3bc6b5f7e1ace9a1fc2
RouZERO <rodrigo.alan.m.c@live.com>
2021-08-23 10:13:00 -0300

    Update 00 - install.sh

--- 00 - install.sh
+++ 00 - install.sh
@@ -5,7 +5,7 @@ set -e
 
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
-echo "pt_BR.UTF-8" >> /etc/locale.gen
+echo "pt_BR.UTF-8 UTF-8"  >> /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
 # echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
@@ -15,7 +15,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 ArchLinux.localdomain ArchLinux" >> /etc/hosts
 echo root:password | chpasswd
 
-pacman -S --noconfirm grub efibootmgr networkmanager wpa_supplicant reflector xdg-user-dirs-qt xdg-utils bluez flatpak os-prober ntfs-3g
+pacman -S --noconfirm grub efibootmgr networkmanager wpa_supplicant reflector xdg-user-dirs xdg-utils bluez flatpak os-prober ntfs-3g
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
