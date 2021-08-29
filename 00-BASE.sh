55ad9c2bcc32bf1af49b01a059ea7a1dd79ccad8
RouZERO <rodrigo.alan.m.c@live.com>
2021-08-29 07:41:09 -0300

    Update 00-BASE.sh

--- 00-BASE.sh
+++ 00-BASE.sh
@@ -4,19 +4,37 @@ set -e
 # Author	:	 Rodrigo Alan M. Carvalho
 ##################################################################################################################
 
-
+# Configuração de Horário.
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
+
+# Configuração de Idioma.
 echo "pt_BR.UTF-8 UTF-8"  >> /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
+
+# Para alterar o nome do computador altere onde esta ArchLinux.
 echo "ArchLinux" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 ArchLinux.localdomain ArchLinux" >> /etc/hosts
-# echo root:password | chpasswd
 
-pacman -S --noconfirm grub efibootmgr networkmanager wpa_supplicant xdg-user-dirs xdg-utils bluez flatpak os-prober ntfs-3g pipewire pipewire-{alsa,jack,media-session,pulse}
+# Para alterar o nome altere onde esta rouzero e a senha altere passsword.
+echo root:password | chpasswd
+
+# Arquivos Básicos.
+pacman -S --noconfirm grub efibootmgr networkmanager wpa_supplicant xdg-user-dirs xdg-utils bluez flatpak os-prober ntfs-3g 
+
+# Para usar o Servidor de som Pipeware.
+# pacman -S --noconfirm pipewire pipewire-{alsa,jack,media-session,pulse}
+
+# Para usar o Servidor de som Pulseaudio.
+pacman -S --noconfirm pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
+
+useradd -m rouzero
+# Para alterar o nome altere onde esta rouzero e a senha altere passsword.
+echo rouzero:password | chpasswd
+echo "rouzero ALL=(ALL) ALL" >> /etc/sudoers.d/rouzero
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -24,12 +42,5 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable fstrim.timer
-
-useradd -m rouzero
-# Para alterar a senha altere onde esta passsword
-echo rouzero:password | chpasswd
-
-echo "rouzero ALL=(ALL) ALL" >> /etc/sudoers.d/rouzero
-
 
 /bin/echo -e "\e[1;32mEtapa Finalizada! Agora Instale um Desktop Environment \e[0m"
