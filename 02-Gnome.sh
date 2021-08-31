37b010b8f6f3286f2d520eb21bd3d59017636056
RouZERO <rodrigo.alan.m.c@live.com>
2021-08-31 11:28:00 -0300

    Update 02-Gnome.sh

--- 02-Gnome.sh
+++ 02-Gnome.sh
@@ -4,7 +4,7 @@ set -e
 # Modicificacão :       Rodrigo Alan M. Carvalho
 
 
-sudo pacman -S gnome-shell gnome-tweak-tool gnome-control-center gnome-keyring nautilus gdm tilix evolution-data-server --noconfirm --needed
+sudo pacman -S gnome-shell gnome-tweak-tool gnome-control-center gnome-keyring nautilus gdm tilix evolution-data-server noto-fonts-cjk --noconfirm --needed
 sudo systemctl enable gdm
 
 
